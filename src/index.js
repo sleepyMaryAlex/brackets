34dfc679543a238861833659ac33d3c85df61283
Maria Guchkova <mariaguchkova1@gmail.com>
2022-01-24 15:27:39 +0300

    feat: implement function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let openingBrackets = [];
+  let closingBrackets = [];
+  for (let arr of bracketsConfig) {
+    openingBrackets.push(arr[0]);
+    closingBrackets.push(arr[1]);
+  }
+  let opening = [];
+  for (let bracket of str) {
+    let brackets = bracketsConfig.find((arr) => arr[0] === bracket || arr[1] === bracket);
+    if (closingBrackets.includes(bracket) && brackets[0] === opening[opening.length - 1]) {
+      opening.pop();
+    } else if (openingBrackets.includes(bracket)) {
+      opening.push(bracket);
+    } else {
+      return false;
+    }
+  }
+  return opening.length === 0;
+};
